c56b1ef141be3b0507af8eeb51125c9648bd4d88
Manav <manavbhatt056@gmail.com>
2024-08-24 19:06:02 +0530

    adding margin to theme button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,7 @@ const items = [
               ))}
             </div>
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 mr-5">
           <ThemeSwitcherBtn />
           <UserButton afterSignOutUrl="/sign-in" />
         </div>
