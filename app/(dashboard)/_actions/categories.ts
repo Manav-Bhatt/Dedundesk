1f58c966b65b5395b6c4ad0c3de56df6442a8745
Manav <manavbhatt056@gmail.com>
2024-10-10 22:31:24 +0530

    manage page implementation

--- app/(dashboard)/_actions/categories.ts
+++ app/(dashboard)/_actions/categories.ts
@@ -4,9 +4,12 @@ import prisma from "@/lib/prisma";
 import {
     CreateCategorySchema,
     CreateCategorySchemaType,
+    DeleteCategorySchema,
+    DeleteCategorySchemaType,
 } from "@/schema/categories";
 
 import { currentUser } from "@clerk/nextjs/server";
+import { Prisma } from "@prisma/client";
 import { redirect } from "next/navigation";
 
 export async function CreateCategory(form: CreateCategorySchemaType) {
@@ -28,6 +31,29 @@ export async function CreateCategory(form: CreateCategorySchemaType) {
             name,
             icon,
             type,
+        },
+    });
+}
+
+export async function DeleteCategory(form: DeleteCategorySchemaType){
+    const parsedBody = DeleteCategorySchema.safeParse(form);
+    if (!parsedBody.success) {
+
+        throw new Error("bad request");
+    }
+
+    const user = await currentUser();
+    if (!user) {
+        redirect("/sign-in");
+    }
+
+    return await prisma.category.delete({
+        where:{
+            name_userId_type: {
+                userId: user.id,
+                name: parsedBody.data.name,
+                type: parsedBody.data.type,
+            },
         },
     });
 }
